f0ef025e7d8052c6da3942acee79ff23e049b4c4
Grimidk <thegrimidk@gmail.com>
2024-09-30 15:06:06 -0400

    directories

--- ProjectApplevDell/src/projectClasses/manager.java
+++ ProjectApplevDell/src/projectClasses/manager.java
@@ -2,18 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package projectApp;
-import ProjectClasses.*;
-import ProjectTools.*;
-import projectInterface.init;
+package projectClasses;
 
 /**
  *
  * @author juanmendezl
  */
-public class app {
-    public static void start(){
-        init ini = new init();
-        ini.setVisible(true);
-    }
+public class manager {
+    
 }
