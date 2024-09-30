e38264ed810a25fc51d5e2a88bd378be0274a650
Grimidk <thegrimidk@gmail.com>
2024-09-29 21:01:09 -0400

    main

---
+++ ProjectApplevDell/src/projectApp/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectApp;
+//import ProjectClasses.*;
+//import ProjectTools.*;
+import projectInterface.init;
+
+/**
+ *
+ * @author juanmendezl
+ */
+public class App {
+    public static void start(){
+        init ini = new init();
+        ini.setVisible(true);
+    }
+}
